146f945ea915a56401dba16d2e94cbd69fb23466
Achacha <achachapeter@gmail.com>
2017-06-30 15:23:25 +0300

    concat

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,12 @@
 //Business logic//
 var token = "bd6d25878528c8f00e0f5900e4c037106ec8db91";
 
+
 function Git() {}
 
 exports.getRepos = function(){
   $.get('http://api.github.com/users/daneden?access_token=' + token).then(function(response) {
-    console.log(response);
+    $('#result').add(response);
   }).fail(function(error) {
     console.log(error.responseJSON.message)
   });
